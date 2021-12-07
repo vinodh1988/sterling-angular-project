e4401573402a4ce37c03d0c4a37a3dc51990111e
vinodh1988 <k.c.vinodh@hotmail.com>
2021-12-07 17:00:42 +0530

    input-output-attribute-directives

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./box.component.css']
 })
 export class BoxComponent implements OnInit {
+theme:string="theme1"
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggle(theme:string){
+    this.theme=theme
   }
 
 }
